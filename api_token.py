f4ddc436fdda8eafd67b138de8a096c2f6082ebc
pedro092692 <pedro_092692@hotmail.com>
2024-10-08 13:07:01 -0400

    add token class and get token function

---
+++ api_token.py
@@ -0,0 +1,35 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+# load env
+load_dotenv()
+
+
+class Token:
+
+    def __init__(self):
+        # get client_id and client_secret from environ
+        self.client_id = os.environ.get('CLIENT_ID')
+        self.client_secret = os.environ.get('CLIENT_SECRET')
+
+        # prepare url to get access token
+        self.url = (f'https://id.twitch.tv/oauth2/token?'
+                    f'client_id={self.client_id}&client_secret={self.client_secret}&grant_type=client_credentials')
+
+        self.token = ''
+
+
+
+    def get_token(self):
+        try:
+            response = requests.post(url=self.url)
+            response.raise_for_status()
+            result = response.json()
+        except requests.exceptions.HTTPError:
+            print('Sorry there was a problem obtaining access token')
+            return False
+        else:
+            return result
+
+
